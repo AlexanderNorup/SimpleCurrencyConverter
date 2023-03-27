750989d5a40b9aa42df1e021382cc51a8be37b14
Alexander Nørup <me@alexandernorup.com>
2023-03-27 21:22:35 +0200

    First prototype done.. Please just be the only version ever, thank you...

---
+++ SimpleCurrencyConverter/SimpleCurrencyConverter/ExchangeViewState.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleCurrencyConverter
+{
+    public class ExchangeViewState
+    {
+        public Currency SelectedInputCurrency { get; set; }
+        public Currency SelectedOutputCurrency { get; set; }
+        public double LastSalesTaxValue { get; set; }
+        public bool UseSalesTax { get; set; }
+    }
+}
