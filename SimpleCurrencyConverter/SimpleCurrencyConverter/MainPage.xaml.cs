d9e5a5124e912b6f1ce484c8be5106d38e4de0c4
Alexander Nørup <me@alexandernorup.com>
2023-03-27 21:22:12 +0200

    Updated logo

--- SimpleCurrencyConverter/SimpleCurrencyConverter/MainPage.xaml.cs
+++ SimpleCurrencyConverter/SimpleCurrencyConverter/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Globalization;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -18,11 +19,117 @@ namespace SimpleCurrencyConverter
             InitializeComponent();
             _stateManager = new StateManager();
             var salesTaxStates = _stateManager.GetSalesTax().Select(x => new SalesTaxEntry(x.Key, x.Value)).OrderBy(x => x.StateName).ToList();
-            //ExchangeRates = _stateManager.GetExchangeRates();
             SalesTaxStatePicker.ItemsSource = salesTaxStates;
             SalesTaxStatePicker.SelectedIndexChanged += StateSelectionChanged;
 
             UpdateRatesBtn.Clicked += UpdateExchangeRatesClicked;
+            InputValue.TextChanged += (o, e) => DoCalculations();
+            InputCurrency.SelectedIndexChanged += (o, e) => { UpdateState(); DoCalculations(); };
+            OutputCurrency.SelectedIndexChanged += (o, e) => { UpdateState(); DoCalculations(); };
+            SalesTax.TextChanged += (o, e) => { UpdateState(); DoCalculations(); };
+            UseSalesTax.CheckedChanged += (o, e) => { UpdateState(); DoCalculations(); };
+
+            SwapCurrenciesBtn.Clicked += SwapCurrencies;
+            NewInputBtn.Clicked += NewInput;
+
+            _ = UpdateExchangeRates(false);
+        }
+
+        private void NewInput(object sender, EventArgs e)
+        {
+            InputValue.Text = string.Empty;
+            InputValue.Focus();
+        }
+
+        private void SwapCurrencies(object sender, EventArgs e)
+        {
+            var inputCurrency = InputCurrency.SelectedIndex;
+            var outputCurrency = OutputCurrency.SelectedIndex;
+            var outputValueArr = OutputValue.Text.Split(' ');
+            var outputValue = outputValueArr.Length > 1 ? outputValueArr[1] : string.Empty;
+            var inputValie = InputValue.Text;
+
+            InputValue.Text = outputValue;
+            OutputValue.Text = inputValie;
+            InputCurrency.SelectedIndex = outputCurrency;
+            OutputCurrency.SelectedIndex = inputCurrency;
+
+            DoCalculations();
+            UpdateState();
+        }
+
+        private void UpdateState()
+        {
+            if (double.TryParse(SalesTax.Text, out var salesTax))
+            {
+                _stateManager.ExchangeViewState.LastSalesTaxValue = salesTax;
+            }
+
+            if (InputCurrency.SelectedItem is Currency inputCurrency)
+            {
+                _stateManager.ExchangeViewState.SelectedInputCurrency = inputCurrency;
+            }
+
+            if (OutputCurrency.SelectedItem is Currency outputCurrency)
+            {
+                _stateManager.ExchangeViewState.SelectedOutputCurrency = outputCurrency;
+            }
+
+            _stateManager.ExchangeViewState.UseSalesTax = UseSalesTax.IsChecked;
+
+            _stateManager.SaveState();
+        }
+
+        private void ApplySavedState()
+        {
+            SalesTax.Text = _stateManager.ExchangeViewState.LastSalesTaxValue.ToString(CultureInfo.InvariantCulture);
+            UseSalesTax.IsChecked = _stateManager.ExchangeViewState.UseSalesTax;
+
+            InputCurrency.SelectedIndex = InputCurrency.ItemsSource.IndexOf(_stateManager.ExchangeViewState.SelectedInputCurrency);
+            OutputCurrency.SelectedIndex = OutputCurrency.ItemsSource.IndexOf(_stateManager.ExchangeViewState.SelectedOutputCurrency);
+        }
+
+        private void DoCalculations()
+        {
+            if (!double.TryParse(InputValue.Text, out var inputValue))
+            {
+                OutputValue.Text = "";
+                return;
+            }
+
+            var inputCurrency = InputCurrency.SelectedItem as Currency;
+            if (inputCurrency is null)
+            {
+                OutputValue.Text = "";
+                return;
+            }
+
+            var outputCurrency = OutputCurrency.SelectedItem as Currency;
+            if (outputCurrency is null)
+            {
+                OutputValue.Text = "";
+                return;
+            }
+
+            double salesTaxRate = 0;
+            if (UseSalesTax.IsChecked && !double.TryParse(SalesTax.Text, out salesTaxRate))
+            {
+                OutputValue.Text = "! Invalid Sales Tax !";
+                return;
+            }
+
+            var dkkInputValue = inputValue * (inputCurrency.ParsedRate / 100d);
+            var salesTax = salesTaxRate * dkkInputValue;
+
+            var dkkTotal = dkkInputValue + salesTax;
+
+            var totalConvertedToOutput = dkkTotal;
+            if (outputCurrency.Code != "DKK")
+            {
+                totalConvertedToOutput = dkkTotal / (outputCurrency.ParsedRate / 100d);
+            }
+
+            OutputValue.Text = outputCurrency.Code + " " + totalConvertedToOutput.ToString("0.00", CultureInfo.InvariantCulture);
         }
 
         private async void UpdateExchangeRatesClicked(object sender, EventArgs e)
@@ -32,31 +139,49 @@ namespace SimpleCurrencyConverter
 
         private async Task UpdateExchangeRates(bool force)
         {
-            if (force)
+            ExchangeRateLbl.Text = "Fetching rates...";
+            try
             {
-                ExchangeRates = await _stateManager.ForceFetchExchangeRates();
-            }
-            else
-            {
-                ExchangeRates = await _stateManager.GetExchangeRates();
-            }
+                if (force)
+                {
+                    ExchangeRates = await _stateManager.ForceFetchExchangeRates();
+                }
+                else
+                {
+                    ExchangeRates = await _stateManager.GetExchangeRates();
+                }
 
-            var currencies = ExchangeRates.Currencies.Select(x => x.Value).ToList();
-            currencies.Add(new Currency()
+                var currencies = ExchangeRates.Currencies.Select(x => x.Value).ToList();
+                currencies.Add(new Currency()
+                {
+                    Desc = "Danske Kroner",
+                    Code = "DKK",
+                    Rate = "100.0"
+                });
+
+                var inputSelectedIndex = InputCurrency.SelectedIndex;
+                var outputSelectedIndex = OutputCurrency.SelectedIndex;
+
+                InputCurrency.ItemsSource = currencies;
+                OutputCurrency.ItemsSource = currencies;
+
+                InputCurrency.SelectedIndex = inputSelectedIndex;
+                OutputCurrency.SelectedIndex = outputSelectedIndex;
+
+                ExchangeRateLbl.Text = "Exchange rates from: " + ExchangeRates.EffectiveDate.ToLongDateString();
+                ApplySavedState();
+            }
+            catch (Exception ex)
             {
-                Desc = "Danske Kroner",
-                Code = "DKK",
-                Rate = "100.0"
-            });
-            InputCurrency.ItemsSource = currencies;
-            OutputCurrency.ItemsSource = currencies;
+                ExchangeRateLbl.Text = "Failed to fetch Exchange-rates: " + ex.Message;
+            }
         }
 
         private void StateSelectionChanged(object sender, EventArgs e)
         {
-            SalesTax.Text = (SalesTaxStatePicker.SelectedItem as SalesTaxEntry).Rate.ToString(CultureInfo.InvariantCulture);
+            SalesTax.Text = (SalesTaxStatePicker.SelectedItem as SalesTaxEntry)?.Rate.ToString(CultureInfo.InvariantCulture) ?? "<unparseable item>";
+            DoCalculations();
         }
-
 
         public ExchangeRates ExchangeRates { get; set; }
     }
--- SimpleCurrencyConverter/SimpleCurrencyConverter/StateManager.cs
+++ SimpleCurrencyConverter/SimpleCurrencyConverter/StateManager.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
@@ -12,13 +13,27 @@ namespace SimpleCurrencyConverter
 {
     public class StateManager
     {
+        private const string ExchangeViewStateFileName = "exchangeStateView.json";
+        private const string ExchangeRatesFileName = "exchangeRates.json";
+
         private const string BaseApiAddress = "https://www.alexandernorup.com/";
         private const string ExchangeRatesApiEndpoint = "res/exchangeRates.php";
         private ExchangeRates m_exchangeRates;
         private Dictionary<string, JsonSalesTax> m_salesTax;
 
+        public ExchangeViewState ExchangeViewState { get; set; } = new ExchangeViewState();
+
         public StateManager()
         {
+            TryParseJsonFile<ExchangeViewState>(ExchangeViewStateFileName, out var viewState);
+            if (viewState != null)
+            {
+                ExchangeViewState = viewState;
+            }
+
+            TryParseJsonFile(ExchangeRatesFileName, out m_exchangeRates);
+
+            //Since sales tax is embedded, you have to read it another way. 
             var salesTaxContent = ReadAssemblyJson("sales_tax_rates.json");
             var rawSalesTax = JsonConvert.DeserializeObject<Dictionary<string, JsonSalesTax>>(salesTaxContent);
 
@@ -31,6 +46,15 @@ namespace SimpleCurrencyConverter
                 }
             }
             m_salesTax = filteredSalexTax;
+        }
+
+        public void SaveState()
+        {
+            var exchangeRatesPath = GetAccessibleFolderPath(ExchangeRatesFileName);
+            var exchangeViewStatePath = GetAccessibleFolderPath(ExchangeViewStateFileName);
+
+            File.WriteAllText(exchangeRatesPath, JsonConvert.SerializeObject(m_exchangeRates));
+            File.WriteAllText(exchangeViewStatePath, JsonConvert.SerializeObject(ExchangeViewState));
         }
 
         public async Task<ExchangeRates> GetExchangeRates()
@@ -56,6 +80,7 @@ namespace SimpleCurrencyConverter
                     }
 
                     m_exchangeRates = JsonConvert.DeserializeObject<ExchangeRates>(await response.Content.ReadAsStringAsync());
+                    SaveState();
                     return m_exchangeRates;
                 }
             }
@@ -76,6 +101,31 @@ namespace SimpleCurrencyConverter
                 jsonString = reader.ReadToEnd();
             }
             return jsonString;
+        }
+
+        private bool TryParseJsonFile<T>(string filename, out T output) where T : class
+        {
+            output = null;
+            var filePath = GetAccessibleFolderPath(filename);
+            if (File.Exists(filePath))
+            {
+                var fileContents = File.ReadAllText(filePath);
+                if (fileContents.Trim() != string.Empty)
+                {
+                    var parsedJson = JsonConvert.DeserializeObject<T>(fileContents);
+                    if (parsedJson != null)
+                    {
+                        output = parsedJson;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private string GetAccessibleFolderPath(string filename)
+        {
+            return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), filename);
         }
     }
 }
